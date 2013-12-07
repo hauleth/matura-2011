f7206472ae9a5a5469959b036fb195bf7afbc3c2
Łukasz Niemier <lukasz@niemier.pl>
2013-12-07 18:00:11 +0100

    Update matura.scm

--- matura.scm
+++ matura.scm
@@ -10,7 +10,10 @@
                (if (= (+ n 1) (length ret))
                  ret
                  (let* ((tmp (/ iter 2))
-                        (ret (append ret (list (+ (- iter 1) (list-ref ret (ceiling tmp)) (list-ref ret (floor tmp)))))))
+                        (ret (append ret (list (+
+                                                 (- iter 1)
+                                                 (list-ref ret (ceiling tmp))
+                                                 (list-ref ret (floor tmp)))))))
                    (loop (+ iter 1) ret))))))
     (loop 2 '(1 1))))
 
@@ -19,9 +22,4 @@
     (display n)
     (newline)))
 
-;(let loop ((i 1))
-;  (if (< i 10001)
-;    (begin
-;      (puts (sklej i)) ; Display result of recursive version
-;      (loop (+ i 1)))))
 (for-each puts (sklej-gen 10000)) ; Display generated list
